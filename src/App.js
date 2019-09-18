98c02874c33489e5445453d87664c2a60c25dbeb
Kyle Hall <kyle.d.hall@accenture.com>
2019-09-18 14:47:10 -0500

    set up root App component and Title component using styled-components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Title from './components/Title'
+
+const App = props => (
+    <div>
+        <Title>Styled-Components Lab</Title>
+        <hr style={{color: 'hsl(166, 65%, 52%)'}}/>
+    </div>
+)
+
+export default App
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default styled.h1`
+    font-size: 2em;
+    text-align: center;
+    font-family: sans-serif;
+    color: hsl(166, 65%, 52%);
+`
